e3c8b0d09e131a3b7e6b8a855709276987edbb48
William Stark <william@ebi.ac.uk>
2020-05-18 16:31:39 +0100

    save command to generate a BLAST database

---
+++ generate_blast_database.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+#gene_database="homo_sapiens_core_101_38"
+#gene_database="mus_musculus_core_101_38"
+gene_database="pan_paniscus_core_101_1"
+
+
+input_file_path="data/$gene_database.fasta"
+
+
+# get the gene symbols from the gene database and save them to a file if missing
+if [[ ! -f "$input_file_path" ]]; then
+    perl get_gene_symbols.pl -dbname "$gene_database" > "$input_file_path"
+fi
+
+
+# -dbtype <String, `nucl', `prot'>
+#   Molecule type of target db
+dbtype="prot"
+
+# -in <File_In>
+#   Input file/database name
+#   Default = `-'
+in="$input_file_path"
+
+# -out <String>
+#   Name of BLAST database to be created
+#   Default = input file name provided to -in argumentRequired if multiple
+#   file(s)/database(s) are provided as input
+out="databases/$gene_database"
+
+makeblastdb -dbtype $dbtype -in $in -out $out
