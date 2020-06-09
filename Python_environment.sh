c37fcac59ba352e2f7eb55256b51360ec707c800
William Stark <william@ebi.ac.uk>
2020-06-09 15:38:08 +0100

    import Python_environment.sh

---
+++ Python_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# load with source <file_path>
+
+
+# required for importing torch
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/lib64:/nfs/software/ensembl/RHEL74-dev/linuxbrew/lib"
