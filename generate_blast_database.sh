01e77d38a670170ed175350575ed4d57f3c7c43d
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-06-09 13:12:11 +0100

    update script for the new data file

--- generate_blast_database.sh
+++ generate_blast_database.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 
-gene_database="homo_sapiens_core_101_38"
+#gene_database="homo_sapiens_core_101_38"
 #gene_database="mus_musculus_core_101_38"
 
 
-input_file_path="data/$gene_database.fasta"
+#input_file_path="data/$gene_database.fasta"
 
 
 # get the gene symbols from the gene database and save them to a file if missing
-if [[ ! -f "$input_file_path" ]]; then
-    perl get_gene_symbols.pl -dbname "$gene_database" > "$input_file_path"
-fi
+# if [[ ! -f "$input_file_path" ]]; then
+#     perl get_gene_symbols.pl -dbname "$gene_database" > "$input_file_path"
+# fi
+
+
+gene_database="all_species"
+input_file_path="data/$gene_database.fa"
 
 
 # -dbtype <String, `nucl', `prot'>
@@ -27,6 +31,6 @@ in="$input_file_path"
 #   Name of BLAST database to be created
 #   Default = input file name provided to -in argumentRequired if multiple
 #   file(s)/database(s) are provided as input
-out="data/blast_databases/$gene_database"
+out="data/blast_database/$gene_database"
 
 makeblastdb -dbtype $dbtype -in $in -out $out
