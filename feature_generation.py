36c582add94dda46275e361ad15c656dfa6567cc
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-07-29 16:09:15 +0100

    move BLAST results parsing to feature_generation.py

--- feature_generation.py
+++ feature_generation.py
@@ -10,7 +10,11 @@ Feature generation.
 
 
 # standard library imports
+import argparse
+import csv
+import io
 import pathlib
+import shelve
 import sys
 
 # third party imports
@@ -53,11 +57,59 @@ def select_to_fasta():
     dataframe_to_fasta(most_frequent_100, fasta_path)
 
 
+def parse_blast_results(blast_results):
+    """
+    """
+    # open blast_results string as a file for easy consumption by csv.reader
+    with io.StringIO(blast_results) as file_object:
+        fieldnames = [
+                "query_id", "subject_id", "percent_identity", "alignment_length", "mismatches", "gap_opens", "query_start", "query_end", "subject_start", "subject_end", "evalue", "bit_score"
+                ]
+        for row in csv.DictReader(file_object, fieldnames=fieldnames, delimiter="\t"):
+            print(row)
+            break
+
+
+def split_fasta_sequence(fasta_sequence):
+    parts = fasta_sequence.split()
+    description = parts[0].replace(">", "")
+    sequence = parts[1]
+
+    return description, sequence
+
+
+def generate_blast_features():
+    """
+    Parse raw BLAST results and generate a dictionary with important values.
+    """
+    # shelve_db_path = data_directory / "most_frequent_100-blast_results.db"
+    shelve_db_path = data_directory / "blast_results_sample.db"
+
+    with shelve.open(str(shelve_db_path)) as blast_results:
+        print("loading blast_results database...")
+        print()
+
+        for fasta_sequence, blast_output in blast_results.items():
+            description, sequence = split_fasta_sequence(fasta_sequence)
+            print(description)
+            print()
+            blast_features = parse_blast_results(blast_output)
+            break
+
+
 def main():
     """
     main function
     """
-    select_to_fasta()
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("--select_to_fasta", action="store_true")
+
+    args = argument_parser.parse_args()
+
+    if args.select_to_fasta:
+        select_to_fasta()
+    else:
+        generate_blast_features()
 
 
 if __name__ == "__main__":
