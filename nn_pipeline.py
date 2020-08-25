310437f22e3c26c641934489bd8f4a3ca0fc188b
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-08-25 16:00:00 +0100

    create training and test sets

--- nn_pipeline.py
+++ nn_pipeline.py
@@ -12,12 +12,19 @@ Neural network pipeline.
 # standard library imports
 import pathlib
 import pickle
+import sys
 
 # third party imports
+import pandas as pd
 import torch
+import sklearn
+
+from sklearn.model_selection import train_test_split
 
 # project imports
 
+
+RANDOM_STATE = None
 
 data_directory = pathlib.Path("data")
 
@@ -36,14 +43,34 @@ def train_model():
     with open(blast_features_pickle_path, "rb") as f:
         blast_features = pickle.load(f)
 
-    print(blast_features)
-    print(len(blast_features))
+    # split features and labels
+    features = [value["blast_values"] for value in blast_features.values()]
+    labels = [value["symbol"] for value in blast_features.values()]
+    # num_items = 1
+    num_items = 3
+
+    # shuffle examples
+    features, labels = sklearn.utils.shuffle(features, labels, random_state=RANDOM_STATE)
+
+    # split examples to training and test sets
+    train_features, test_features, train_labels, test_labels = train_test_split(
+        features, labels, test_size=0.3, random_state=RANDOM_STATE
+    )
+
+    num_train = len(train_features)
+    num_test = len(test_features)
+    print(f"dataset split to {num_train} training and {num_test} test samples")
+    print()
 
 
 def main():
     """
     main function
     """
+    # DEBUG
+    # pd.options.display.max_columns = None
+    # pd.options.display.max_rows = None
+
     train_model()
 
 
