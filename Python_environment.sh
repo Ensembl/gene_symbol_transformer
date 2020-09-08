45d04ed5a02bfaaba2590e2b4fc0c6c4b803b94a
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-09-08 12:27:59 +0100

    use direnv and a .envrc to load environment variables for this project

--- Python_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# load with `source <file_path>`
-# i.e. `source Python_environment.sh`
-
-
-# required for importing torch
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/lib64:/nfs/software/ensembl/RHEL74-dev/linuxbrew/lib"
