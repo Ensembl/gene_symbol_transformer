3989ef2e71ec10ca9a35d9e485de082712440124
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-09-18 10:29:26 +0100

    use a variable to specify the type of shell; add option to specify the queue to submit

--- dev_scripts/open_bsub_shell.sh
+++ dev_scripts/open_bsub_shell.sh
@@ -17,16 +17,38 @@
 # Submits a parallel job and specifies the number of tasks in the job.
 # -M mem_limit [!]
 # Sets a memory limit for all the processes that belong to the job.
+# -q "queue_name ..."
+# Submits the job to one of the specified queues.
 # -R "res_req" [-R "res_req" ...]
 # Runs the job on a host that meets the specified resource requirements.
 
+#JOB_TYPE=standard
+#JOB_TYPE=parallel
+JOB_TYPE=gpu
 
-#MIN_TASKS=8
-#MIN_TASKS=16
+#QUEUE=research-rh74
+QUEUE=production-rh74
 
-MEM_LIMIT=16384
-#MEM_LIMIT=32768
+#MEM_LIMIT=16384
+MEM_LIMIT=32768
 #MEM_LIMIT=65536
 
-#bsub -Is -tty -n $MIN_TASKS -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" $SHELL
-bsub -Is -tty -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" $SHELL
+#MIN_TASKS=8
+MIN_TASKS=16
+
+
+# open a shell on a stardard compute node
+if [[ "$JOB_TYPE" = "standard" ]]; then
+    bsub -Is -tty -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" $SHELL
+fi
+
+# open a parallel jobs shell
+if [[ "$JOB_TYPE" = "parallel" ]]; then
+    bsub -Is -tty -n $MIN_TASKS -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" $SHELL
+fi
+
+# open a shell on a GPU node
+# https://sysinf.ebi.ac.uk/doku.php?id=ebi_cluster_good_computing_guide#gpu_hosts
+if [[ "$JOB_TYPE" = "gpu" ]]; then
+    bsub -q $QUEUE -P gpu -gpu - -Is -tty -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" $SHELL
+fi
