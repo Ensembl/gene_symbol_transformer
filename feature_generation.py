58eecb40bdd279c5143459610906888b02b61ee8
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-09-24 14:36:40 +0100

    reorganize dataset generation code

--- feature_generation.py
+++ feature_generation.py
@@ -18,94 +18,14 @@ import shelve
 import sys
 
 # third party imports
-import Bio
 import pandas as pd
 
 # project imports
-import dataset_generation
 
 
 USE_CACHE = True
 
 data_directory = pathlib.Path("data")
-
-
-def split_fasta_sequence(fasta_sequence):
-    parts = fasta_sequence.split()
-    description = parts[0].replace(">", "")
-    sequence = parts[1]
-
-    return description, sequence
-
-
-def get_protein_letters():
-    """
-    Generate and return a list of protein letters that occur in the dataset and
-    those that can potentially be used.
-    """
-    extended_IUPAC_protein_letters = Bio.Alphabet.IUPAC.ExtendedIUPACProtein.letters
-
-    # cache the following operation, as it's very expensive in time and space
-    if USE_CACHE:
-        extra_letters = ["*"]
-    else:
-        data = dataset_generation.load_data()
-
-        # generate a list of all protein letters that occur in the dataset
-        dataset_letters = set(data["sequence"].str.cat())
-
-        extra_letters = [
-            letter
-            for letter in dataset_letters
-            if letter not in extended_IUPAC_protein_letters
-        ]
-
-    protein_letters = list(extended_IUPAC_protein_letters) + extra_letters
-    assert len(protein_letters) == 27, protein_letters
-
-    return protein_letters
-
-
-def dataframe_to_fasta(dataframe, fasta_path):
-    """
-    Generate a FASTA file from a genes dataframe.
-    """
-    print("generating FASTA file from dataframe...")
-    with open(fasta_path, "w+") as fasta_file:
-        for entry in dataframe.itertuples():
-            entry_dict = entry._asdict()
-
-            symbol = entry_dict["symbol"]
-            stable_id = entry_dict["stable_id"]
-            sequence = entry_dict["sequence"]
-
-            fasta_file.write(f">{stable_id};{symbol}\n{sequence}\n")
-    print(f"FASTA file saved at {fasta_path}")
-
-
-def save_most_frequent_n(n, max_frequency=None):
-    """
-    Save the examples of the n most frequent symbols to a pickled dataframe and
-    a FASTA file.
-
-    Specify the max_frequency of the n symbols to run an extra validation check.
-    """
-    data = dataset_generation.load_data()
-
-    symbol_counts = data["symbol"].value_counts()
-
-    if max_frequency is not None:
-        assert all(symbol_counts[:n] == symbol_counts[symbol_counts >= max_frequency])
-
-    most_frequent_n = data[data["symbol"].isin(symbol_counts[:n].index)]
-
-    # save dataframe to a pickle file
-    pickle_path = data_directory / f"most_frequent_{n}.pickle"
-    most_frequent_n.to_pickle(pickle_path)
-
-    # save sequences to a FASTA file
-    fasta_path = data_directory / f"most_frequent_{n}.fasta"
-    dataframe_to_fasta(most_frequent_n, fasta_path)
 
 
 def parse_blast_output(query_id, blast_output_raw, sequence):
@@ -235,8 +155,6 @@ def main():
     # pd.options.display.max_rows = None
 
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("--save_most_frequent_101", action="store_true")
-    argument_parser.add_argument("--save_most_frequent_3", action="store_true")
     argument_parser.add_argument(
         "--generate_blast_features_most_frequent_101", action="store_true"
     )
@@ -246,10 +164,6 @@ def main():
 
     args = argument_parser.parse_args()
 
-    if args.save_most_frequent_101:
-        save_most_frequent_n(n=101, max_frequency=297)
-    elif args.save_most_frequent_3:
-        save_most_frequent_n(n=3, max_frequency=335)
     elif args.generate_blast_features_most_frequent_101:
         generate_blast_features_most_frequent_n(n=101)
     elif args.generate_blast_features_most_frequent_3:
