7d6a28baddfba3b096de51a464ea04070e12f8a3
Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-10-13 19:02:02 +0100

    add variable to select between types of gpu nodes
    exit and print help message if run without arguments

--- dev_scripts/submit_training.sh
+++ dev_scripts/submit_training.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+if [[ -z "$1" ]]; then
+    echo "Nothing to run, pass the command to run with bsub as an argument."
+    exit
+fi
+
+
 DATE_TIME=$(date +%Y-%m-%d_%H:%M:%S%:z)
 
 JOB_NAME="$DATE_TIME"
@@ -9,16 +15,21 @@ JOB_NAME="$DATE_TIME"
 #QUEUE=research-rh74
 QUEUE=production-rh74
 
-#MEM_LIMIT=16384
-#MEM_LIMIT=20000
-#MEM_LIMIT=32768
-MEM_LIMIT=65536
+GPU_NODE="V100"
+#GPU_NODE="any"
 
-#bsub -q $QUEUE -P gpu -gpu "num=1:j_exclusive=yes" -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" -o "${JOB_NAME}.stdout.txt" -e "${JOB_NAME}.stderr.txt" "$@"
+MEM_LIMIT=16384
+#MEM_LIMIT=32768
+#MEM_LIMIT=65536
+
+if [[ "$GPU_NODE" = "any" ]]; then
+    bsub -q $QUEUE -P gpu -gpu "num=1:j_exclusive=yes" -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" -o "${JOB_NAME}.stdout.txt" -e "${JOB_NAME}.stderr.txt" "$@"
+fi
 
 # specify compute node
-COMPUTE_NODE=gpu-009
-#COMPUTE_NODE=gpu-010
-#COMPUTE_NODE=gpu-011
+#COMPUTE_NODE=gpu-009
+COMPUTE_NODE=gpu-011
 
-bsub -q $QUEUE -P gpu -gpu "num=1:j_exclusive=yes" -m ${COMPUTE_NODE}.ebi.ac.uk -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" -o "${JOB_NAME}.stdout.txt" -e "${JOB_NAME}.stderr.txt" "$@"
+if [[ "$GPU_NODE" = "V100" ]]; then
+    bsub -q $QUEUE -P gpu -gpu "num=1:j_exclusive=yes" -m ${COMPUTE_NODE}.ebi.ac.uk -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" -o "${JOB_NAME}.stdout.txt" -e "${JOB_NAME}.stderr.txt" "$@"
+fi
