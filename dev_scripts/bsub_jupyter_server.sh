ce5047d54f449e086699ede4be301ed24ecc8000
William Dimitrios Paraschas <dimitrios@ebi.ac.uk>
2020-10-30 22:51:13 +0000

    remove obsolete scripts

--- dev_scripts/bsub_jupyter_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-MEM_LIMIT=16384
-
-PORT=54321
-
-bsub -Is -tty -q production-rh74 -M $MEM_LIMIT -R"select[mem>$MEM_LIMIT] rusage[mem=$MEM_LIMIT] span[hosts=1]" jupyter lab --no-browser --port=$PORT
