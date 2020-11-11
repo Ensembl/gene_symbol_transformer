6f731e12299feb154676da6706af33ed1ba8be40
William Stark <william@ebi.ac.uk>
2020-11-11 23:07:28 +0000

    add license and licensing notice

--- submit_training.sh
+++ submit_training.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 
+# Copyright 2020 EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 #NUM_MOST_FREQUENT_SYMBOLS=3
 #NUM_MOST_FREQUENT_SYMBOLS=101
 #NUM_MOST_FREQUENT_SYMBOLS=1013
