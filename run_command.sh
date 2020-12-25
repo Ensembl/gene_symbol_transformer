521a37ed1a02bdbc689c7b9dc7eddf8a879802c9
William Stark <dimitrios@ebi.ac.uk>
2020-12-25 18:12:56 +0000

    pass the pipeline script as an argument, check if it is a valid one

--- run_command.sh
+++ run_command.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 
+#PYTHON_SCIPT="lstm_pipeline.py"
+#PYTHON_SCIPT="cnn_pipeline.py"
+PYTHON_SCIPT="$1"
+
+if [[ "$PYTHON_SCIPT" != "lstm_pipeline.py" && "$PYTHON_SCIPT" != "cnn_pipeline.py" ]]; then
+    echo "pass one of the pipeline scripts as an argument: {\"lstm_pipeline.py\", \"cnn_pipeline.py\"}"
+    exit
+fi
+
 DATETIME=$(date +%Y-%m-%d_%H:%M)
 
 #NUM_MOST_FREQUENT_SYMBOLS=3
@@ -12,8 +21,6 @@ NUM_MOST_FREQUENT_SYMBOLS=1013
 #NUM_MOST_FREQUENT_SYMBOLS=30591
 
 RANDOM_STATE=5
-
-PYTHON_SCIPT="lstm_pipeline.py"
 
 # train directly on a GPU node, disable buffering in Python script output, save output to a file with tee
 python -u "$PYTHON_SCIPT" --datetime "$DATETIME" --random_state $RANDOM_STATE --num_most_frequent_symbols $NUM_MOST_FREQUENT_SYMBOLS --train --test | tee -a "networks/n=${NUM_MOST_FREQUENT_SYMBOLS}_${DATETIME}.log"
