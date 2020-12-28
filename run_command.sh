6156bb267222f3aec582e0b26c78a3421258a975
William Stark <dimitrios@ebi.ac.uk>
2020-12-28 12:23:45 +0000

    improve checking for valid pipeline script

--- run_command.sh
+++ run_command.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/env bash
 
 
-#PYTHON_SCIPT="lstm_pipeline.py"
-#PYTHON_SCIPT="cnn_pipeline.py"
-PYTHON_SCIPT="$1"
+#TARGET_SCRIPT="lstm_pipeline.py"
+#TARGET_SCRIPT="cnn_pipeline.py"
+#TARGET_SCRIPT="fully_connected_pipeline.py"
+TARGET_SCRIPT="$1"
 
-if [[ "$PYTHON_SCIPT" != "lstm_pipeline.py" && "$PYTHON_SCIPT" != "cnn_pipeline.py" ]]; then
-    echo "pass one of the pipeline scripts as an argument: {\"lstm_pipeline.py\", \"cnn_pipeline.py\"}"
+
+PIPELINE_SCRIPTS=(
+    "lstm_pipeline.py"
+    "cnn_pipeline.py"
+    "fully_connected_pipeline.py"
+)
+
+VALID_SCRIPT=false
+for PIPELINE_SCRIPT in "${PIPELINE_SCRIPTS[@]}"; do
+    if [[ "$TARGET_SCRIPT" = "$PIPELINE_SCRIPT" ]]; then
+        VALID_SCRIPT=true
+    fi
+done
+
+if [[ $VALID_SCRIPT != true ]]; then
+    echo "pass one of the pipeline scripts as an argument:"
+    echo "{"
+    for PIPELINE_SCRIPT in "${PIPELINE_SCRIPTS[@]}"; do
+        echo "    \"$PIPELINE_SCRIPT\","
+    done
+    echo "}"
     exit
 fi
 
 DATETIME=$(date +%Y-%m-%d_%H:%M:%S)
 
-#NUM_MOST_FREQUENT_SYMBOLS=3
+NUM_MOST_FREQUENT_SYMBOLS=3
 #NUM_MOST_FREQUENT_SYMBOLS=101
 #NUM_MOST_FREQUENT_SYMBOLS=1013
-NUM_MOST_FREQUENT_SYMBOLS=10059
+#NUM_MOST_FREQUENT_SYMBOLS=10059
 #NUM_MOST_FREQUENT_SYMBOLS=20147
 #NUM_MOST_FREQUENT_SYMBOLS=25028
 #NUM_MOST_FREQUENT_SYMBOLS=30591
@@ -23,4 +43,4 @@ NUM_MOST_FREQUENT_SYMBOLS=10059
 RANDOM_STATE=5
 
 # train directly on a GPU node, disable buffering in Python script output, save output to a file with tee
-python -u "$PYTHON_SCIPT" --datetime "$DATETIME" --random_state $RANDOM_STATE --num_most_frequent_symbols $NUM_MOST_FREQUENT_SYMBOLS --train --test | tee -a "networks/n=${NUM_MOST_FREQUENT_SYMBOLS}_${DATETIME}.log"
+python -u "$TARGET_SCRIPT" --datetime "$DATETIME" --random_state $RANDOM_STATE --num_most_frequent_symbols $NUM_MOST_FREQUENT_SYMBOLS --train --test | tee -a "networks/n=${NUM_MOST_FREQUENT_SYMBOLS}_${DATETIME}.log"
