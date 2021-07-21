788b212481903f943df7248b61c949003bd31069
William Stark <william@ebi.ac.uk>
2021-07-21 15:32:17 +0100

    add core dbs to Rapid Release assemblies metadata; save metadata as a pickle file

--- rapid_release_assignments.py
+++ rapid_release_assignments.py
@@ -28,8 +28,12 @@ import argparse
 import json
 import pathlib
 import sys
+import time
 
 # third party imports
+import pandas as pd
+import pymysql
+
 from loguru import logger
 
 # project imports
@@ -38,6 +42,7 @@ from utils import (
     PrettySimpleNamespace,
     data_directory,
     download_file,
+    get_ensembl_release,
     load_checkpoint,
     logging_format,
     sequences_directory,
@@ -66,25 +71,100 @@ def get_rapid_release_assemblies_metadata():
     The metadata are loaded from the `species_metadata.json` file in the Rapid Release
     FTP root directory.
     """
+    assemblies_metadata_path = data_directory / "rapid_release_assemblies_metadata.pickle"
+    if assemblies_metadata_path.exists():
+        assemblies_metadata_df = pd.read_pickle(assemblies_metadata_path)
+        assemblies_metadata = [
+            PrettySimpleNamespace(**values.to_dict())
+            for index, values in assemblies_metadata_df.iterrows()
+        ]
+        return assemblies_metadata
+
+    logger.info("generating Rapid Release genome assemblies metadata")
+
     # download the species metadata file
-    assemblies_metadata_filename = "species_metadata.json"
-    assemblies_metadata_url = (
-        f"http://ftp.ensembl.org/pub/rapid-release/{assemblies_metadata_filename}"
+    assemblies_metadata_json_filename = "species_metadata.json"
+    assemblies_metadata_json_url = (
+        f"http://ftp.ensembl.org/pub/rapid-release/{assemblies_metadata_json_filename}"
     )
     data_directory.mkdir(exist_ok=True)
-    assemblies_metadata_path = data_directory / assemblies_metadata_filename
-    if not assemblies_metadata_path.exists():
-        download_file(assemblies_metadata_url, assemblies_metadata_path)
-        logger.info(f"downloaded {assemblies_metadata_path}")
+    assemblies_metadata_json_path = data_directory / assemblies_metadata_json_filename
+    if not assemblies_metadata_json_path.exists():
+        download_file(assemblies_metadata_json_url, assemblies_metadata_json_path)
+        logger.info(f"downloaded {assemblies_metadata_json_path}")
 
-    with open(assemblies_metadata_path) as f:
+    with open(assemblies_metadata_json_path) as f:
         assemblies_metadata = json.load(f)
 
+    gca_to_core_dbs = get_rapid_release_core_dbs()
+
+    for assembly in assemblies_metadata:
+        assembly["core_db"] = gca_to_core_dbs[assembly["assembly_accession"]]
+
+    assemblies_metadata_df = pd.DataFrame(assemblies_metadata)
+    assemblies_metadata_df.to_pickle(assemblies_metadata_path)
+    logger.info(f"dataset metadata saved at {assemblies_metadata_path}")
+
     assemblies_metadata = [
-        PrettySimpleNamespace(**assembly) for assembly in assemblies_metadata
+        PrettySimpleNamespace(**values.to_dict())
+        for index, values in assemblies_metadata_df.iterrows()
     ]
 
     return assemblies_metadata
+
+
+def get_rapid_release_core_dbs(
+    host="mysql-ens-mirror-5",
+    port=4692,
+    user="ensro",
+):
+    """
+    Get assembly accessions to core database names for Rapid Release.
+    """
+    ensembl_release = get_ensembl_release()
+
+    connection = pymysql.connect(
+        host=host,
+        port=port,
+        user=user,
+    )
+    get_core_databases_sql = f"""
+    SHOW DATABASES LIKE '%core_{ensembl_release}%';
+    """
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(get_core_databases_sql)
+            core_databases = cursor.fetchall()
+
+    assembly_accession_to_core_databases = {}
+    for core_database in core_databases:
+        # delay between SQL queries
+        time.sleep(0.1)
+
+        assert len(core_database) == 1, f"{core_database}"
+        core_database = core_database[0]
+
+        connection = pymysql.connect(
+            host=host,
+            port=port,
+            user=user,
+            database=core_database,
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+
+        get_assembly_accessions_sql = """
+        SELECT meta_value FROM meta WHERE meta_key='assembly.accession';
+        """
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(get_assembly_accessions_sql)
+                assembly_accession = cursor.fetchall()
+        assert len(assembly_accession) == 1, f"{assembly_accession}"
+        assembly_accession = assembly_accession[0]["meta_value"]
+
+        assembly_accession_to_core_databases[assembly_accession] = core_database
+
+    return assembly_accession_to_core_databases
 
 
 def main():
