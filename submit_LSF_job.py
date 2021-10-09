7fd27713a117def5672c545f803a4d7e9d72b46e
William Stark <william@ebi.ac.uk>
2021-10-09 10:11:50 +0100

    store the optimizer as a separate object in the checkpoint

--- submit_LSF_job.py
+++ submit_LSF_job.py
@@ -99,7 +99,9 @@ def main():
     elif args.checkpoint:
         checkpoint_path = pathlib.Path(args.checkpoint)
 
-        experiment, _network, _symbols_metadata = load_checkpoint(checkpoint_path)
+        experiment, _network, _optimizer, _symbols_metadata = load_checkpoint(
+            checkpoint_path
+        )
 
         num_symbols = experiment.num_symbols
 
