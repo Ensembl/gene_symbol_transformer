24b38aef1ead4c15cea0045242a6da332ba6d7ac
William Stark <william@ebi.ac.uk>
2021-10-27 15:05:27 +0100

    add plot with ground truth

--- notebooks/single_genome_assignments_stats.py
+++ notebooks/single_genome_assignments_stats.py
@@ -6,24 +6,30 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.3
+#       jupytext_version: 1.12.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
 # %% [markdown]
-# # symbol assignments probability threshold analysis
+# # symbol genome assignments statistics
 
 # %%
 import pathlib
 
-from gsc_stats import plot_threshold_statistics_no_ground_truth
+from gsc_stats import plot_threshold_statistics, plot_threshold_statistics_no_ground_truth
 
 # %%
-comparison_csv_path = pathlib.Path("../../assignments/salmo_salar_gca905237065v2_core_104_1_protein_sequences_symbols.csv")
+comparison_csv = ""
+comparison_csv_path = pathlib.Path(comparison_csv)
 
+# %%
+print(comparison_csv_path.stem)
+plot_threshold_statistics(comparison_csv_path, text_title=True)
+
+# %%
 print(comparison_csv_path.stem)
 plot_threshold_statistics_no_ground_truth(comparison_csv_path, text_title=True)
 
