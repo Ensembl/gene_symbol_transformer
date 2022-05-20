9c619dcc0156fda5cc9c2f5d83528ff33f7e1a8c
William Stark <william@ebi.ac.uk>
2022-05-20 18:36:39 +0100

    get max field lengths for tab files, modularize populating tables

--- orthologs_database_schema.sql
+++ orthologs_database_schema.sql
@@ -43,21 +43,29 @@ CREATE TABLE odb10v1_OGs (
 -- Ortho DB genes with some info
 CREATE TABLE odb10v1_genes (
   -- 1. Ortho DB unique gene id (not stable between releases)
-  gene_id CHAR(32),
+  -- max string length: 16
+  gene_id CHAR(16),
   -- 2. Ortho DB individual organism id
+  -- max string length: 9
   organism_id CHAR(16),
   -- 3. protein original sequence id, as downloaded along with the sequence
+  -- max string length: 40
   sequence_id CHAR(64),
   -- 4. semicolon separated list of synonyms, evaluated by mapping
-  synonyms VARCHAR(1024),
+  -- max string length: 1039
+  synonyms VARCHAR(2048),
   -- 5. Uniprot id, evaluated by mapping
-  uniprot_id CHAR(64),
+  -- max string length: 10
+  uniprot_id CHAR(16),
   -- 6. semicolon separated list of ids from Ensembl, evaluated by mapping
-  ensembl_ids VARCHAR(1024),
+  -- max string length: 2222
+  ensembl_ids VARCHAR(4096),
   -- 7. NCBI gid or gene name, evaluated by mapping
-  ncbi_gid_or_gene_name CHAR(64),
+  -- max string length: 9
+  ncbi_gid_or_gene_name CHAR(16),
   -- 8. description, evaluated by mapping
-  description VARCHAR(10240),
+  -- max string length: 383
+  description VARCHAR(512),
 
   PRIMARY KEY (gene_id)
 );
@@ -66,20 +74,25 @@ CREATE TABLE odb10v1_genes (
 -- UniProt, ENSEMBL, NCBI, GO and InterPro ids associated with Ortho DB gene
 CREATE TABLE odb10v1_gene_xrefs (
   -- 1. Ortho DB gene id
-  gene_id CHAR(32),
+  -- max string length: 16
+  gene_id CHAR(16),
   -- 2. external gene identifier, either mapped or the original sequence id from Genes table
+  -- max string length: 43
   external_id CHAR(64),
   -- 3. external DB name, one of {GOterm, InterPro, NCBIproteinGI, UniProt, ENSEMBL, NCBIgid, NCBIgenename}
+  -- max string length: 14
   external_db CHAR(16)
 );
 
 
 -- OGs to genes correspondence
 CREATE TABLE odb10v1_OG2genes (
+  -- max string length: 17
   -- 1. OG unique id
   og_id CHAR(32),
   -- 2. Ortho DB gene id
-  gene_id CHAR(32)
+  -- max string length: 16
+  gene_id CHAR(16)
 );
 
 
