c67c86bf0c4fb94fbd3677fef0c3a76f30558fd9
William Stark <william@ebi.ac.uk>
2022-05-26 10:41:39 +0100

    docs

--- orthologs_database_schema.sql
+++ orthologs_database_schema.sql
@@ -1,20 +1,29 @@
--- See the NOTICE file distributed with this work for additional information
--- regarding copyright ownership.
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+/*
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 
--- reference: https://v101.orthodb.org/download/README.txt
+/*
+SQL schema for the [OrthoDB](https://www.orthodb.org/) release files derived database.
+
+MySQL style SQL that is compatible with SQLite is being used.
+
+release files: https://www.orthodb.org/?page=filelist
+reference: https://v101.orthodb.org/download/README.txt
+*/
 
 
 DROP TABLE IF EXISTS odb10v1_OGs;
