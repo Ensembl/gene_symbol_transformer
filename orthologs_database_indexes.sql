1d1a9e73e59e11cc9c7f47559fe65b7fc7563972
William Stark <william@ebi.ac.uk>
2022-06-01 10:34:23 +0100

    optimize database using the ANALYZE command

--- orthologs_database_indexes.sql
+++ orthologs_database_indexes.sql
@@ -1,3 +1,21 @@
+/*
+See the NOTICE file distributed with this work for additional information
+regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 -- generate indexes
 
 DROP INDEX IF EXISTS og_id_odb10v1_OGs_index;
@@ -23,8 +41,3 @@ CREATE INDEX gene_id_odb10v1_gene_xrefs_index ON odb10v1_gene_xrefs(gene_id);
 
 -- odb10v1_all_og_fasta
 CREATE INDEX gene_id_odb10v1_all_og_fasta_index ON odb10v1_all_og_fasta(gene_id);
-
-
--- -- speeds computation 1000x in some cases
--- logger.info("Running ANALYZE features")
--- ANALYZE features;
